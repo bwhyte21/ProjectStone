3607809525c926d60a5a78f798d35434b7ce8527
Bryan Whyte <bryan.whyte@acendre.com>
2021-07-26 16:59:18 -0400

    Add project files.

---
+++ ProjectStone/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectStone.Models;
+
+namespace ProjectStone.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+      {
+          
+      }
+
+      public DbSet<Category> Category { get; set; }
+  }
+}
---
+++ ProjectStone/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectStone.Models
+{
+  public class Category
+  {
+      // Data annotations "[Key]" for table to identify primary key. To keep it explicit, use [Key].
+      [Key]
+      public int Id { get; set; }
+
+      public string Name { get; set; }
+      public int DisplayOrder { get; set; }
+  }
+}
