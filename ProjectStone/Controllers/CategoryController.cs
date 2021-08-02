90848d2c4e5b954fe41c1b315c9dd9576b5ccb81
Bryan Whyte <bwhyte21@outlook.com>
2021-08-02 18:56:49 -0400

    Edit action added.

--- ProjectStone/Controllers/CategoryController.cs
+++ ProjectStone/Controllers/CategoryController.cs
@@ -52,5 +52,23 @@ namespace ProjectStone.Controllers
 
             return View(obj);
         }
+
+        // GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id ==0)
+            {
+                return NotFound();
+            }
+
+            // Find only works on primary key.
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            
+            return View(obj);
+        }
     }
 }
