b2cbf09e0d25a07801eee13cacbba94a30f29aea
Bryan Whyte <bwhyte21@outlook.com>
2021-08-03 13:27:02 -0400

    Product class added via EF

--- ProjectStone/Data/ApplicationDbContext.cs
+++ ProjectStone/Data/ApplicationDbContext.cs
@@ -7,14 +7,12 @@ using ProjectStone.Models;
 
 namespace ProjectStone.Data
 {
-  public class ApplicationDbContext : DbContext
-  {
-      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-      {
-          
-      }
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-      public DbSet<Category> Category { get; set; }
-      public DbSet<SubCategory> SubCategory { get; set; }
-  }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
 }
--- ProjectStone/Migrations/20210803171751_AddProductToDb.Designer.cs
+++ ProjectStone/Migrations/20210803171751_AddProductToDb.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectStone.Data;
 
 namespace ProjectStone.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210803171751_AddProductToDb")]
+    partial class AddProductToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
