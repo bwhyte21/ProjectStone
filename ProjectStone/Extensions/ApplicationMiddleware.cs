6dd297fa50dca359d3f6e6e174b6c2aa439bb1ca
Bryan Whyte <bwhyte21@outlook.com>
2021-10-28 19:14:51 -0400

    Cleaned up Startup.cs.

---
+++ ProjectStone/Extensions/ApplicationMiddleware.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ProjectStone_DataAccess.Initializer;
+
+namespace ProjectStone.Extensions
+{
+  public static class ApplicationMiddleware
+  {
+      public static IApplicationBuilder AddApplicationMiddleware(this IApplicationBuilder app, IWebHostEnvironment env, IConfiguration config, IDbInitializer dbInitializer)
+      {
+          #region SyncFusion License Config
+
+          // Register Syncfusion License.
+          Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(config["SyncFusion:LicenseKey"]);
+
+          #endregion
+
+          if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+          else
+          {
+              app.UseExceptionHandler("/Home/Error");
+              // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+              app.UseHsts();
+          }
+
+          app.UseHttpsRedirection();
+
+          // Use files in wwwRoot folder.
+          app.UseStaticFiles();
+
+          app.UseRouting();
+
+          #region Identity Pipeline Config
+
+          // Now that we're using Identity, we must add UseAuthentication BEFORE UseAuthorization.
+          // Note: After migrating Identity into the DB, right-click the Project, go to "Add", click "Add New Scaffolded Item", then select Identity to choose
+          // then add in the desired Identity Pages (Logins, ForgotPassword, etc)
+          app.UseAuthentication();
+
+          #endregion
+
+          app.UseAuthorization();
+
+          #region DBInitializer Pipeline Config
+
+          // Invoke the initializer located in DataAccess.
+          dbInitializer.Initialize();
+
+          #endregion
+
+          #region Session Pipeline Config
+
+          // Extra Session service extensions are to be located in a "Utility" folder.
+          app.UseSession();
+
+          #endregion
+
+          app.UseEndpoints(endpoints =>
+          {
+              // Routing for Razor Pages. Needed after Scaffolding Identity Razor Pages.
+              endpoints.MapRazorPages();
+              // Routing for MVC.
+              endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+          });
+
+          return app;
+      }
+  }
+}
--- ProjectStone/Startup.cs
+++ ProjectStone/Startup.cs
@@ -1,23 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ProjectStone_DataAccess.Data;
-using ProjectStone_DataAccess.Repository;
-using ProjectStone_DataAccess.Repository.IRepository;
-using ProjectStone_Utility;
-using System;
-using Microsoft.AspNetCore.Http;
+using ProjectStone.Extensions;
 using ProjectStone_DataAccess.Initializer;
-using ProjectStone_Utility.BrainTree;
 
 namespace ProjectStone
 {
-    public class Startup
+  public class Startup
     {
         // Make the IConfiguration instance private instead of the provided public one.
         private readonly IConfiguration _config;
@@ -30,149 +21,24 @@ namespace ProjectStone
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            #region DBContext Services
-
-            // Adding a DbContext Service.
-            // This uses the connection string that was created in appsettings.json file. (Dependency Injection)
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
-
-            #endregion
-
-            #region Identity Services
-
-            // Add Identity service. Install NuGet pkg Microsoft.AspNetCore.Identity.UI
-            // .AddEntityFrameworkStores creates the identity tables in our DB for us.
-            // Add identityRole to config Role Manager while using "AddIdentity".
-            //services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>(); // We use the one below since we've added roles.
-
-            // .AddDefaultTokenProviders for when "Forgot Password" is invoked. [Needed along with Identity Role]
-            // .AddDefaultUI is for Identity pages and more. [Needed along with Identity Role]
-            services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI().AddEntityFrameworkStores<ApplicationDbContext>();
-
-            #endregion
-
-            #region Email Services
-
-            // Email service registration.
-            services.AddTransient<IEmailSender, EmailSender>();
-
-            #endregion
-
-            #region Session Services
-
-            // Add Sessions to this .Net Core project.
-            services.AddHttpContextAccessor();
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(10);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.SameSite = SameSiteMode.Lax;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                options.Cookie.IsEssential = true;
-            });
-
-            #endregion
-
-            #region Braintree Services for 3rd Party Payments using Dependency Injection
-
-            // Configure BrainTree settings based on appsettings.json file. This method works the same as how MailJet was setup, but this way is cleaner.
-            //  Everything should be registered ONCE, here, in the container, Startup.cs
-            services.Configure<BrainTreeSettings>(_config.GetSection("BrainTree"));
-            services.AddSingleton<IBrainTreeGate, BrainTreeGate>();
-
-            #endregion
-
-            #region Repository Services for Dependency Injection
-
-            // Register Repository containers here.
-            // We will use AddScoped for databases to be used for the scope's lifetime. Meaning it will stay for one request. 
-            // If we use it multiple times, it will still use the same object.
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<ISubCategoryRepository, SubCategoryRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<IInquiryHeaderRepository, InquiryHeaderRepository>();
-            services.AddScoped<IInquiryDetailRepository, InquiryDetailRepository>();
-            services.AddScoped<IOrderHeaderRepository, OrderHeaderRepository>();
-            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
-            services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
-
-            // This service will be for the DbInitializer with the purpose of Seeding the DB in a new environment for deployment.
-            // This will also be added to the pipeline config below.
-            services.AddScoped<IDbInitializer, DbInitializer>();
-
-            #endregion
-
-            #region Facebook Service Authentication
-
-            // Register Facebook SSOAuth service to project.
-            services.AddAuthentication().AddFacebook(options =>
-            {
-                options.AppId = _config["FaceBook:AppId"];
-                options.AppSecret = _config["FaceBook:AppSecret"];
-            });
-
-            #endregion
+            #region Controller Service
 
             services.AddControllersWithViews();
+
+            #endregion
+
+            #region Other Services
+
+            services.AddApplicationServices(_config);
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
-            #region SyncFusion License Config
-
-            // Register Syncfusion License.
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(_config["SyncFusion:LicenseKey"]);
-
-            #endregion
-
-            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            // Use files in wwwRoot folder.
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            #region Identity Pipeline Config
-
-            // Now that we're using Identity, we must add UseAuthentication BEFORE UseAuthorization.
-            // Note: After migrating Identity into the DB, right-click the Project, go to "Add", click "Add New Scaffolded Item", then select Identity to choose
-            // then add in the desired Identity Pages (Logins, ForgotPassword, etc)
-            app.UseAuthentication();
-
-            #endregion
-
-            app.UseAuthorization();
-
-            #region DBInitializer Pipeline Config
-
-            // Invoke the initializer located in DataAccess.
-            dbInitializer.Initialize();
-
-            #endregion
-
-            #region Session Pipeline Config
-
-            // Extra Session service extensions are to be located in a "Utility" folder.
-            app.UseSession();
-
-            #endregion
-
-            app.UseEndpoints(endpoints =>
-            {
-                // Routing for Razor Pages. Needed after Scaffolding Identity Razor Pages.
-                endpoints.MapRazorPages();
-                // Routing for MVC.
-                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            // Configure HTTP Request pipeline.
+            app.AddApplicationMiddleware(env, _config, dbInitializer);
         }
     }
 }
